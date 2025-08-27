82cddfc1a25122417fe91366a8c5449e9011efd7
Filipe <filipesilveira.dev@gmail.com>
2025-08-27 16:35:41 -0300

    Explicações do código

--- semestre 1/xadrez/xadrez.c
+++ semestre 1/xadrez/xadrez.c
@@ -6,7 +6,7 @@ int main(){
 	int k = 0;
 	
 	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
-	printf("A torre anda: \n");
+	printf("A Torre anda: \n");
 		//equanto a condicional (k<5) for verdadeira, o código se repetirá
 		while(k < 5){
 		//a cada repetição do código, será impressa a mensagem
@@ -19,7 +19,7 @@ int main(){
 	int j = 0;
 	
 	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
-	printf("\nO bispo anda: \n");
+	printf("\nO Bispo anda: \n");
 	//no caso do "do", o código será executado pelo menos uma vez
 	do{
 		//a cada repetição do código, será impressa a mensagem
@@ -29,7 +29,7 @@ int main(){
 	} while (j < 5);
 	
 	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
-	printf("\nA rainha anda: \n");
+	printf("\nA Rainha anda: \n");
 	
 	//no caso de for, a variável contadora, a condicional e o incremento são declarados dentro dos parênteses
 	for (int i = 0; i < 8; i++){
@@ -38,17 +38,22 @@ int main(){
 	}
 	
 		//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
-	printf("\nO cavalo anda: \n");
+	printf("\nO Cavalo anda: \n");
 	
+	//variável utilizada no loop externo
 	int l = 0;
 	
+	//como o laço "do", garanto uma execução do código antes da condicional
 	do{
+		//loop interno será executado duas vezes
 		for(int m = 0; m < 2; m++){
+			//será mostrada duas vezes "baixo" antes de finalizar o loop interno
 			printf("baixo\n");
 		}
-		//a cada repetição do código, será impressa a mensagem
+		//após sair do laço interno, será impressa esta mensagem pelo laço externo. 
 		printf("Esquerda\n");
-		//incremento da variável contadore. A cada repetição, o valor de j aumenta 1
+		//incremento da variável contadora. A cada repetição, o valor de l aumenta 1
 		l++;
+	//condição para acabar o loop externo. Ou seja, o loop externo será executado apenas uma vez
 	} while (l < 1);
 }
