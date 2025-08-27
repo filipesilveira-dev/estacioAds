3b9e255ae2adc253b1da1e76b6a62d08d633de14
Filipe <filipesilveira.dev@gmail.com>
2025-08-27 10:40:03 -0300

    Peças de xadrez nível aventureiro

--- semestre 1/xadrez/xadrez.c
+++ semestre 1/xadrez/xadrez.c
@@ -2,5 +2,38 @@
 #include <locale.h>	//biblioteca que permite configurar localização por meio de "setlocale()"
 
 int main(){
+	//vari[avel contadora para a peça torre
+	int k = 0;
 	
+	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
+	printf("A torre anda: \n");
+		//equanto a condicional (k<5) for verdadeira, o código se repetirá
+		while(k < 5){
+		//a cada repetição do código, será impressa a mensagem
+		printf("Direita\n");
+		//incremento da variável contadore. A cada repetição, o valor de k aumenta 1
+		k++;
+	}
+	
+	//vari[avel contadora para a peça bispo
+	int j = 0;
+	
+	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
+	printf("\nO bispo anda: \n");
+	//no caso do "do", o código será executado pelo menos uma vez
+	do{
+		//a cada repetição do código, será impressa a mensagem
+		printf("cima, direita\n");
+		//incremento da variável contadore. A cada repetição, o valor de j aumenta 1
+		j++;
+	} while (j < 5);
+	
+	//antes do laço de repetição é impressa a mensagem (para não repetir todas as vezes)
+	printf("\nA rainha anda: \n");
+	
+	//no caso de for, a variável contadora, a condicional e o incremento são declarados dentro dos parênteses
+	for (int i = 0; i < 8; i++){
+		////a cada repetição do código, será impressa a mensagem
+		printf("Esquerda\n");
+	}
 }
