70944cf21e45a97e13fdfce1b1b87651dcba1256
Filipe <filipesilveira.dev@gmail.com>
2025-09-01 21:58:12 -0300

    Desafio nível mestre ok

--- semestre 1/batalhaNaval/batalhaNaval.c
+++ semestre 1/batalhaNaval/batalhaNaval.c
@@ -1,6 +1,8 @@
+//declaração de bibliotecas necessárias para ciração e bom funcionamento do código
 #include <stdio.h>
 #include <locale.h>	//biblioteca que permite configurar localização por meio de "setlocale()"
 
+//constantes declaradas para utilizar nas matrizes de habilidade/ataque as quais possuem as mesmas dimensões
 #define LINHA 3
 #define COLUNA 5
 
@@ -18,6 +20,7 @@
 		{0,0,0,0,0,0,0,0,0,0}
 	};
 	
+	//declaração de matrizes (utilizando as constantes) que representarão os ataques
 	int habilidadeCone[LINHA][COLUNA];
 	int habilidadeCruz[LINHA][COLUNA];
 	int habilidadeOctaedro[LINHA][COLUNA];
@@ -25,50 +28,41 @@
 	//vetor que mostra a marcação superior das posições
 	char linhas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 	
-	
+	//função que repreenta o ataque em forma de cruz
 	int ataqueCruz(){
+		//loopr externo controla as linhas
 		for (int i =0; i<LINHA; i++){
-		//loop interno controlando as colunas
+			//loop interno controlando as colunas
 			for (int j =0; j<COLUNA; j++){
-			//atribui a cada posição da matrizSoma a soma do valor de cada elemento das outras duas matrizes na posição equivalente			
+			//ponto de iníco do ataque definido como o tabuleiro[0][5]. A partir desse ponto, será realizada uma soma de matrizes (tabuleiro e habilidadeCruz)			
 			tabuleiro[0 + i][5 + j] += habilidadeCruz [i][j];
-			//if(i > 8) || (j > 7){
-			//	habilidadeCruz[i][j]
-			//}
-			//mostra em tela a matrizSoma e seus valores
-			//printf(“matrizSoma[%d][%d] = %d\n”, i, j, matrizSoma[i][j];
 			}
 		}
+		//indica o resultado como verdadeiro
 		return 1;
 	}
-	
+	//função que representa o ataque em forma de cone
 	int ataqueCone(){
+		//loop externo controla as linhas
 		for (int i =0; i<LINHA; i++){
 		//loop interno controlando as colunas
 			for (int j =0; j<COLUNA; j++){
-				//atribui a cada posição da matrizSoma a soma do valor de cada elemento das outras duas matrizes na posição equivalente			
+				//ponto de iníco do ataque definido como o tabuleiro[7][3]. A partir desse ponto, será realizada uma soma de matrizes (tabuleiro e 
+				//habilidadeCone)			
 				tabuleiro[7 + i][3 + j] += habilidadeCone [i][j];
-			//if(i > 8) || (j > 7){
-			//	habilidadeCruz[i][j]
-			//}
-			//mostra em tela a matrizSoma e seus valores
-			//printf(“matrizSoma[%d][%d] = %d\n”, i, j, matrizSoma[i][j];
 			}
 		}
 		return 1;
 	}
 	
 	int ataqueOctaedro(){
+		
 		for (int i =0; i<LINHA; i++){
 		//loop interno controlando as colunas
 			for (int j =0; j<COLUNA; j++){
-				//atribui a cada posição da matrizSoma a soma do valor de cada elemento das outras duas matrizes na posição equivalente			
+				//ponto de iníco do ataque definido como o tabuleiro[3][0]. A partir desse ponto, será realizada uma soma de matrizes (tabuleiro e 
+				//habilidadeOctaedro)			
 				tabuleiro[3 + i][0 + j] += habilidadeOctaedro [i][j];
-			//if(i > 8) || (j > 7){
-			//	habilidadeCruz[i][j]
-			//}
-			//mostra em tela a matrizSoma e seus valores
-			//printf(“matrizSoma[%d][%d] = %d\n”, i, j, matrizSoma[i][j];
 			}
 		}
 		return 1;
@@ -76,7 +70,9 @@
 	
 	//função criada para verificar se há sobreposição de navios partindo da premissa de que haverá sobreposição em casos que o valor de determinada
 	//posição seja maior que 3, sugerindo que o valor "3" de um elemento de navio está sendo colocado na mesmo posição de um outro elemento de valor
-	//"3" de outro navio
+	//"3" de outro navio.
+	
+	//obs: o valor foi alterado de "3" para "6" para não ficar acusando sobreposição sem ter por conta dos valores das matrizes de ataque
 	void sobreposicaoNavio(){
 	//variável de controle para única exibição de "Sobreposição detectada em: " inicialmente como "0" falso
 	int sobreposicaoDetectada = 0;
@@ -85,8 +81,8 @@
 		for (int i = 0; i < 10; i++){
 			//laço interno para controle de colunas
 			for (int j = 0; j < 10; j++){
-				//condicional caso o valor de determinada posição seja maior que "3", indicando msobreposição
-				if (tabuleiro[i][j] > 3){
+				//condicional caso o valor de determinada posição seja maior ou igual a "6", indicando sobreposição
+				if (tabuleiro[i][j] >= 6){
 					//a condicional nega sobreposicaoDetectada (inicialmente "falso"), ou seja, transforma em "verdadeiro", indicando que o trecho
 					//de código dentro da condicional deve ser executado
 					if (!sobreposicaoDetectada){
@@ -102,7 +98,7 @@
 			}
 		}
 	}
-	
+	//função criada para mostrar o tabuleiro "zerado" (sem navios e sem ataques)
 	void imprimirTabuleiro(){
 	printf("*** Tabuleiro Batalha Naval ***\n\n");
 	//mostra os itens do vetor linhas para fazer a marcação superior
@@ -182,18 +178,19 @@ int main (){
 
 	
 	//cria uma matriz 5x5, onde o primeiro elemento é "1" e, à medida em que o laço continua, aumenta uma unidade
-	//loop externo controla as linhas
+	
 	printf("\nHabilidade cruz\n");
+	//loop externo controla as linhas
 	for(int i = 0; i < LINHA; i++){
 		//loop interno controla as colunas
 		for(int j = 0; j < COLUNA; j++){
 			//a cada posição o valor do elemento à "soma" mais "1"
 			habilidadeCruz[i][j] = 0;
 			if(j == 2){
-				habilidadeCruz[i][j] = 1;
+				habilidadeCruz[i][j] = 5;
 			}
 			if(i == 1){
-			habilidadeCruz[i][j] = 1;
+			habilidadeCruz[i][j] = 5;
 			}
 			//mostra cada valor de acordo com a posição
 			printf("%d ", habilidadeCruz[i][j]);
@@ -209,22 +206,22 @@ int main (){
 			//a cada posição o valor do elemento à "soma" mais "1"
 			habilidadeCone[i][j] = 0;
 			if(j == 2){
-				habilidadeCone[i][j] = 1;
+				habilidadeCone[i][j] = 4;
 			}
 			
 			if(i == 1){
-				habilidadeCone[i][1] = 1;
-				habilidadeCone[i][2] = 1;
-				habilidadeCone[i][3] = 1;
+				habilidadeCone[i][1] = 4;
+				habilidadeCone[i][2] = 4;
+				habilidadeCone[i][3] = 4;
 			}
 			
 			if(i == 2){
 				
-				habilidadeCone[i][0] = 1;
-				habilidadeCone[i][1] = 1;
-				habilidadeCone[i][2] = 1;
-				habilidadeCone[i][3] = 1;
-				habilidadeCone[i][4] = 1;
+				habilidadeCone[i][0] = 4;
+				habilidadeCone[i][1] = 4;
+				habilidadeCone[i][2] = 4;
+				habilidadeCone[i][3] = 4;
+				habilidadeCone[i][4] = 4;
 			}
 			//mostra cada valor de acordo com a posição
 			printf("%d ", habilidadeCone[i][j]);
@@ -240,13 +237,13 @@ int main (){
 			//a cada posição o valor do elemento à "soma" mais "1"
 			habilidadeOctaedro[i][j] = 0;
 			if(j == 2){
-				habilidadeOctaedro[i][j] = 1;
+				habilidadeOctaedro[i][j] = 2;
 			}
 			
 			if(i == 1){
-				habilidadeOctaedro[i][1] = 1;
-				habilidadeOctaedro[i][2] = 1;
-				habilidadeOctaedro[i][3] = 1;
+				habilidadeOctaedro[i][1] = 2;
+				habilidadeOctaedro[i][2] = 2;
+				habilidadeOctaedro[i][3] = 2;
 			}
 			
 
@@ -256,13 +253,15 @@ int main (){
 		//salta uma linha após terminar a primeira linha e ir para a segunda
 		printf("\n");	
 	}
-
+	//organização em funções na int main (deixa o código mais limpo e legível)
 	ataqueCruz();
 	ataqueCone();
 	ataqueOctaedro();
+	//chama a função de imprimir o tabuleiro só depois dos ataque terem ocorrido
 	imprimirTabuleiro();
 	//chama a função que verificará se há sobreposição e onde
 	sobreposicaoNavio();
 	
+	//tudo ok com o programa
 	return 0;
 }
